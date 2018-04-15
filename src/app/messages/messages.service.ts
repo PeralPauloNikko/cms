c5abf6a6bfa17662110040ace44858bce3d37ba0
driveN_ <per13090@byui.edu>
2018-04-14 18:36:01 -0600

    Lesson 8

--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,31 +1,48 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import {Injectable, EventEmitter, Output} from '@angular/core';
 import { MOCKMESSAGES } from './message-list/MOCKMESSAGES';
 import {Message} from './message.model';
+import {Response, Http} from '@angular/http';
+import 'rxjs/Rx';
 
 @Injectable()
 export class MessagesService {
-  messageChangeEvent = new EventEmitter<Message[]>();
+  jsonUrl: string = 'https://test-eb48c.firebaseio.com/messages.json';
+  @Output() messageChangeEvent = new EventEmitter<Message[]>();
+  //messageChangeEvent = new EventEmitter<Message[]>();
   selectedMessageEvent = new EventEmitter<Message>();
   messages: Message[] = [];
+  maxMsgId: number;
 
-  constructor() {
+  constructor(private http: Http) {
     this.messages = MOCKMESSAGES;
+    this.maxMsgId = this.getMaxId();
+    this.initMessages();
   }
+
   getMessages(): Message[] {
     return this.messages.slice();
   }
-  getMessage(id:string): Message {
-    for (let message of this.messages) {
-      if(message.id === id) {
-        return message;
-      }
-    }
-    return null;
+
+  getMessage(id: string): Message {
+    return this.messages.filter((message: Message) => {
+      return message.id === id;
+    })[0] || null;
   }
+
   addMessage(message: Message) {
     this.messages.push(message);
-    this.messageChangeEvent.emit(this.messages.slice());
+    this.storeMessages();
+    // this.messageChangeEvent.emit(this.messages.slice());
   }
+
+  storeMessages() {
+    // put request overwrites data
+    this.http.put(this.jsonUrl, JSON.stringify(this.messages))
+      .subscribe(() => {
+        this.messageChangeEvent.next(this.getMessages());
+      });
+  }
+
   addMessages(messages: Message[]) {
     for (let message of messages) {
       this.addMessage(message);
@@ -33,9 +50,33 @@ export class MessagesService {
     this.messages.push(...messages);
     this.messageChangeEvent.emit(this.messages.slice());
   }
+
+  initMessages() {
+    this.http.get(this.jsonUrl)
+    // use the map function
+      .map((response: Response) => {
+        const messages: Message[] = response.json();
+        return messages;
+      })
+
+      .subscribe((messages: Message[]) => {
+        this.messages = messages;
+        this.maxMsgId = this.getMaxId();
+        this.messageChangeEvent.next(this.getMessages());
+      })
+  }
+  getMaxId(): number {
+    let maxId = 0;
+    for (let contact of this.messages){
+      let currentId = +contact.id;
+      if(currentId > maxId){
+        maxId = currentId;
+      }
+    }
+    return maxId;
+  }
+
 }
-
-
 
 
 
